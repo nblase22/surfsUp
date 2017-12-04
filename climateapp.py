b6e375c4bef65b65efcced8cf1be476d9b8972ff
nblase22 <nkmoveout@gmail.com>
2017-12-04 18:48:49 -0500

    tentative complet climateapp, complete part 3

--- climateapp.py
+++ climateapp.py
@@ -1,11 +1,22 @@
-import datetime as dt
-import numpy as np
+# add dependencies
 import pandas as pd
+import os
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+from sqlalchemy.ext.automap import automap_base
+
+from sqlalchemy import Column, Integer, String, Float, and_, Date, desc, func
+
+# PyMySQL 
+import pymysql
+pymysql.install_as_MySQLdb()
 
 from flask import Flask, jsonify
 
@@ -34,10 +45,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-     """List all available api routes."""
+
      return (
          f"Avalable Routes:<br/>"
-         f"/api/v1.0/precipitation <br/>"
+         f"/api/v1.0/precipitation"
          f"- Dates and temperature observations from the last year<br/>"
 
          f"/api/v1.0/stations"
@@ -54,14 +65,47 @@ def welcome():
      )
 
 @app.route("/api/v1.0/precipitation")
+def pcrp():
+    # create the date range, use today as the api here does not take a start/end date
+    today = datetime.datetime.today()
+    today = today.date()
+    last_year = today - datetime.timedelta(365)
+    pcp_year = session.query(Measurement.date, Measurement.prcp, Measurement.tobs).filter(and_(Measurement.date <= today, Measurement.date >= last_year)).all()
+    return jsonify(pcp_year)
 
 @app.route("/api/v1.0/stations")
+def station_list():
+    st_list = session.query(Station.station).all()
+
+    all_stations= list(np.ravel(st_list))
+
+    return jsonify(all_stations)
 
 @app.route("/api/v1.0/tobs")
+def temp_year():
+    # create the date range, use today as the api here does not take a start/end date
+    today = datetime.datetime.today()
+    today = today.date()
+    last_year = today - datetime.timedelta(365)
+    temp_year = session.query(Measurement.date, Measurement.tobs).filter(and_(Measurement.date <= today, Measurement.date >= last_year)).all()
+
+    return jasonify(temp_year)
 
 @app.route("/api/v1.0/<start>")
+def start_temp(start):
+    # get the min/max/avg
+    temp_data = session.query(func.max(Measurement.tobs), func.min(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+    
+    return jsonify(temp_data)
+
+    
 
 @app.route("/api/v1.0/<start>/<end>")
+def range_temp(start, end):
+ # get the min/max/avg
+    temp_data = session.query(func.max(Measurement.tobs), func.min(Measurement.tobs), func.avg(Measurement.tobs)).filter(and_(Measurement.date >= start, Measurement.date <= end)).all()
+    
+    return jsonify(temp_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
